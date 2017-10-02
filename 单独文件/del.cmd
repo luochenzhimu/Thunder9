9d4d0a61047127a7b60b404d1eef4f2f157ee1ba
luochenzhimu <luochenzhimu@outlook.com>
2017-10-02 10:30:56 +0800

    Thunder9
    Thunder9源文件发布

---
+++ 单独文件/del.cmd
@@ -0,0 +1,21 @@
+::无浏览器有设置
+rmdir /s/q 无浏览器有设置\BrowserSupport\layout\CertificateTip
+rmdir /s/q 无浏览器有设置\BrowserSupport\layout\Menu
+rmdir /s/q 无浏览器有设置\BrowserSupport\layout\TabTip
+rmdir /s/q 无浏览器有设置\ThunderCore\layout\TipsManager
+del 无浏览器有设置\ThunderCore\layout\MinMgrWnd\ArrowPanel.xml
+del 无浏览器有设置\ThunderCore\layout\MinMgrWnd\ArrowPanel.xml.lua
+del 无浏览器有设置\ThunderCore\layout\MinMgrWnd\MinMainWnd.xml
+del 无浏览器有设置\ThunderCore\layout\MinMgrWnd\MinMainWnd.xml.lua
+::有浏览器不可折叠下载区
+rmdir /s/q 有浏览器不可折叠下载区\ThunderCore\layout\TipsManager
+del 有浏览器不可折叠下载区\ThunderCore\layout\MinMgrWnd\ArrowPanel.xml
+del 有浏览器不可折叠下载区\ThunderCore\layout\MinMgrWnd\ArrowPanel.xml.lua
+del 有浏览器不可折叠下载区\ThunderCore\layout\MinMgrWnd\MinMainWnd.xml
+del 有浏览器不可折叠下载区\ThunderCore\layout\MinMgrWnd\MinMainWnd.xml.lua
+::有浏览器可折叠下载区
+rmdir /s/q 有浏览器可折叠下载区\ThunderCore\layout\TipsManager
+::ad
+del ad\Offline\layout\command\SceneSession.lua
+del ad\Offline\layout\command\ScenePackages.lua
+pause
